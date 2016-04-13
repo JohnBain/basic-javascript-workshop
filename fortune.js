3c90050f800f8b42b6957fdcea27280336cec4db
jbain1 <jbainschool@gmail.com>
2016-04-13 21:12:18 +0000

    Wrote fortunes

---
+++ fortune.js
@@ -0,0 +1,18 @@
+var fortunes = {
+    1: "It is best to act with confidence, no matter how little right you have to it.",
+    2: "Keep your feet on the ground even though friends flatter you.",
+    3: "Be calm when confronting an emergency crisis.",
+    4: "Enjoy the good luck a companion brings you.",
+    5: "Become who you are.",
+    6: "Tomorrow, take a moment to do something just for yourself.",
+    7: "Be direct, usually one can accomplish more that way.",
+    8: "Adjust finances, make budgets, to improve your standing.",
+    9: "Present your best ideas today to an eager and welcoming audience.",
+    10: "When you're about to turn your heart into a stone remember:\nyou do not walk alone."
+};
+
+console.log(fortunes[Math.floor((Math.random() * 10) + 1)]);
+
+
+
+
